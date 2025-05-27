d219b121080b997fdc6fe9f0130e11a6ef1cb9a3
cukierblaze <wiktoria.migasiewicz@gmail.com>
2025-05-27 12:20:09 +0200

    Update pipeline

--- backend/tests/test_app_import.py
+++ backend/tests/test_app_import.py
@@ -1,6 +1,9 @@
-import pytest
-from backend.app import app as flask_app
+from backend.app import (app as flask_app)
 from flask import Flask
+import os
 
 def test_flask_app_imports_and_is_flask_instance():
+    print('ess')
+    print(os.getcwd())
+    print('ess')
     assert isinstance(flask_app, Flask), "Obiekt 'app' nie jest instancją Flask"
